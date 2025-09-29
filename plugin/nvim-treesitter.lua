140b12d143edad985c133b012048d165d5127fc3
Jack Marken <jackmarken22@gmail.com>
2025-09-29 15:32:33 +1000

    Add commented options for nvim-treesitter

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,12 +1,17 @@
 -- require 'nvim-treesitter.install'.prefer_git = false
 require 'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"javascript", "typescript", "tsx",
+		"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"
+	},
+
 	highlight = {
 		enable = true,
 	},
-	indent = {
-		enable = true,
-	},
+	-- indent = {
+	-- 	enable = true,
+	-- },
 }
 
 -- Use an existing parser for another file type
-vim.treesitter.language.register("html", "ejs")
+-- vim.treesitter.language.register("html", "ejs")
