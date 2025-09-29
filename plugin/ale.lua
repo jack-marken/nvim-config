d0264df06582c1914bb9116e09a0fda0fe22f657
Jack Marken <jackmarken22@gmail.com>
2025-09-29 15:32:49 +1000

    Working ALE linter for React projects

--- plugin/ale.lua
+++ plugin/ale.lua
@@ -1,17 +1,19 @@
+-- vim.g.ale_use_neovim_diagnostics_api = 1
+-- vim.g.ale_virtualtext_cursor = "disabled"
 
-vim.g.ale_fix_on_save = true;
-vim.g.ale_linters_explicit = 1
-vim.g.ale_use_neovim_diagnostics_api = 1
-vim.g.ale_virtualtext_cursor = "disabled"
 vim.g.ale_linters = {
   apkbuild = { "apkbuild_lint" },
   -- TODO: https://github.com/kisielk/errcheck
-  go = { "gopls", "gofmt", "staticcheck", "govet", "golangci-lint" },
-  mail = { "proselint" },
   markdown = { "proselint", "vale" },
+	javascript = { "prettier", "eslint" },
+	javascriptreact = { "prettier", "eslint" },
 	typescript = { "prettier", "eslint" },
 	typescriptreact = { "prettier", "eslint" },
 }
+vim.g.ale_linters_explicit = 1
+vim.g.ale_lint_on_text_changed = 'always'
+vim.g.ale_lint_on_enter = 1
+
 vim.g.ale_fixers = {
   go = { "gofmt" },
   html = { "prettier" },
@@ -20,9 +22,12 @@ vim.g.ale_fixers = {
   markdown = { "prettier" }, -- Disables trim_whitespace. See: https://github.com/dense-analysis/ale/discussions/4640
   scss = { "prettier" },
   sql = { "pgformatter" },
+	javascript = { "prettier", "eslint" },
+	javascriptreact = { "prettier", "eslint" },
   typescript = { "prettier", "eslint" },
   typescriptreact = { "prettier", "eslint" },
   vue = { "prettier" },
   yaml = { "prettier" },
   ["*"] = { "remove_trailing_lines", "trim_whitespace" },
 }
+vim.g.ale_fix_on_save = true;
