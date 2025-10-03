72d8bbae0513bdef0040da82d54beebd04cff9b1
Jack Marken <jackmarken22@gmail.com>
2025-10-04 01:03:11 +1000

    Fix whitespace and formatting

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,24 +1,24 @@
 local function my_on_attach(bufnr)
-	local api = require "nvim-tree.api"
+  local api = require("nvim-tree.api")
 
-	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	end
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
-	-- default mappings
-	api.config.mappings.default_on_attach(bufnr)
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
 
-	-- custom mappings
-	vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
-	vim.keymap.set("n", "s",     api.node.open.horizontal,              opts("Horizontal Split"))
-	vim.keymap.set("n", "v",     api.node.open.vertical,                  opts("Vertical Split"))
+  -- custom mappings
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+  vim.keymap.set("n", "s", api.node.open.horizontal, opts("Horizontal Split"))
+  vim.keymap.set("n", "v", api.node.open.vertical, opts("Vertical Split"))
 end
 
 require("nvim-tree").setup({
-	actions = {
-		open_file = {
-			quit_on_open = true,
-		},
-	},
-	on_attach = my_on_attach,
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  on_attach = my_on_attach,
 })
--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,17 +1,25 @@
 -- require 'nvim-treesitter.install'.prefer_git = false
-require 'nvim-treesitter.configs'.setup {
-	ensure_installed = {
-		"javascript", "typescript", "tsx",
-		"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"
-	},
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "markdown_inline",
+  },
 
-	highlight = {
-		enable = true,
-	},
-	-- indent = {
-	-- 	enable = true,
-	-- },
-}
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+})
 
 -- Use an existing parser for another file type
--- vim.treesitter.language.register("html", "ejs")
+vim.treesitter.language.register("html", "ejs")
