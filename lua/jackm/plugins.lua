5692d70680b06a139e029d499497258ad6cf770e
Jack Marken <jackmarken22@gmail.com>
2025-10-04 00:58:27 +1000

    Reformat whitespace in plugin.lua and add LSP, Formatting, and Linting plugins

--- lua/jackm/plugins.lua
+++ lua/jackm/plugins.lua
@@ -1,54 +1,56 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("airblade/vim-gitgutter")
-	use("tpope/vim-fugitive")
-	use("tpope/vim-commentary")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+  use("wbthomason/packer.nvim")
+  use("airblade/vim-gitgutter")
+  use("tpope/vim-fugitive")
+  use("tpope/vim-commentary")
+  use("JoosepAlviste/nvim-ts-context-commentstring")
 
-	-- Nvim Tree
-	use("nvim-tree/nvim-tree.lua")
-	use("nvim-tree/nvim-web-devicons")
+  -- Nvim Tree
+  use("nvim-tree/nvim-tree.lua")
+  use("nvim-tree/nvim-web-devicons")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    -- or                            , branch = '0.1.x',
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
-	-- Tree Sitter
-	use("nvim-treesitter/nvim-treesitter")
+  -- Tree Sitter
+  use("nvim-treesitter/nvim-treesitter")
 
-	-- LSP
-	use("mason-org/mason.nvim")
-	use("mason-org/mason-lspconfig.nvim")
+  -- LSP
+  use("neovim/nvim-lspconfig")
+  use("mason-org/mason.nvim")
+  use("mason-org/mason-lspconfig.nvim")
+  use("WhoIsSethDaniel/mason-tool-installer.nvim")
 
-	-- Linting
-	use({ "stevearc/conform.nvim" })
-	-- use { "mfussenegger/nvim-lint" }
-	-- use 'mason-org/mason-registry'
-	-- use 'dense-analysis/ale'
+  -- Formatting
+  use("stevearc/conform.nvim")
 
-	-- Colour Schemes
-	use("morhetz/gruvbox")
-	use("savq/melange-nvim")
-	use("rebelot/kanagawa.nvim")
-	use("shaunsingh/nord.nvim")
+  -- Linting
+  use("mfussenegger/nvim-lint")
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  -- Colour Schemes
+  use("morhetz/gruvbox")
+  use("savq/melange-nvim")
+  use("rebelot/kanagawa.nvim")
+  use("shaunsingh/nord.nvim")
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
